244367eb027c2d166307e935a983b576585c6a41
Bouke Haarsma <bouke@haarsma.eu>
2016-12-03 20:54:15 +0100

    Renamed generate command

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Lark",
     targets: [
         Target(name: "CodeGenerator", dependencies: ["SchemaParser"]),
-        Target(name: "soap-generate-code", dependencies: ["SchemaParser", "CodeGenerator"]),
+        Target(name: "lark-generate-client", dependencies: ["SchemaParser", "CodeGenerator"]),
         Target(name: "Demo", dependencies: ["LarkRuntime"]),
     ],
     dependencies: [
