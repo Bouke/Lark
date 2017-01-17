461313d2b2a32bcd5aa36952ab9bf9750ea8c4f6
Bouke Haarsma <bouke@haarsma.eu>
2017-01-17 22:07:02 +0100

    Clean-up unused structures

--- Sources/Lark/Response.swift
+++
@@ -1,8 +0,0 @@
-import Alamofire
-import Foundation
-
-public struct Response<T> {
-    public let request: DataRequest
-    public let response: DataResponse<Envelope>
-    public let result: Alamofire.Result<T>
-}
