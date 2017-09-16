37ba77a4a5ed1f4adf55c8bdb7b977abb7547f84
Bouke Haarsma <bouke@haarsma.eu>
2017-09-16 13:13:10 +0200

    Update package manifest for Swift 4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,20 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "Lark",
-    targets: [
-        Target(name: "CodeGenerator", dependencies: ["Lark", "SchemaParser"]),
-        Target(name: "SchemaParser", dependencies: ["Lark"]),
-        Target(name: "lark-generate-client", dependencies: ["SchemaParser", "CodeGenerator"])
-    ],
     dependencies: [
-        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4)
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.5.0")
     ],
-    swiftLanguageVersions: [3, 4]
+    targets: [
+        .target(name: "Lark", dependencies: ["Alamofire"]),
+        .target(name: "CodeGenerator", dependencies: ["Lark", "SchemaParser"]),
+        .target(name: "SchemaParser", dependencies: ["Lark"]),
+        .target(name: "lark-generate-client", dependencies: ["SchemaParser", "CodeGenerator"]),
+        .testTarget(name: "CodeGeneratorTests", dependencies: ["CodeGenerator"]),
+        .testTarget(name: "LarkTests", dependencies: ["Lark"]),
+        .testTarget(name: "SchemaParserTests", dependencies: ["SchemaParser"]),
+    ],
+    swiftLanguageVersions: [4]
 )
